d4b018e3d355d2c46c09a0977422ee111262e907
lorenzocravero <l.cravero.1295@vallauri.edu>
2021-09-21 18:24:24 +0200

    iniziato secondo es

---
+++ SQL/Ese_02 Aeroporto/Ese_02 Aeroporto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ese_02_Aeroporto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
