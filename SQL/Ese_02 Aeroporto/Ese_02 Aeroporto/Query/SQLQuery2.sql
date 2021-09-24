37c75af672eaaca67c4a06c9f929dc939ecbaa41
lorenzocravero <l.cravero.1295@vallauri.edu>
2021-09-24 10:45:08 +0200

    Esercizio 2 terminato

---
+++ SQL/Ese_02 Aeroporto/Ese_02 Aeroporto/Query/SQLQuery2.sql
@@ -0,0 +1,14 @@
+﻿/* citta di cui non si sa il numero di piste */
+
+SELECT a.Citta
+FROM Aeroporto a
+WHERE a.NumPiste IS NULL
+
+
+/* per ogni volo misto il numero di merci a bordo */
+SELECT v.TipoAereo, v.IdVolo, a.QtaMerci
+FROM Aereo a, VOLO v
+WHERE a.TipoAereo = v.TipoAereo 
+AND a.QtaMerci > 0 AND a.NumPass > 0
+
+/* per ogni 
