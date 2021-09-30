5312fe06a61a22500babed1be903881e77ecd197
lorenzocravero <l.cravero.1295@vallauri.edu>
2021-09-30 16:30:28 +0200

    esercizio 4 dati caricati

---
+++ SQL/Ese_04 Musei/Ese_04 Musei/Query/Inserimento dati.sql
@@ -0,0 +1,5 @@
+﻿/* Inserisco alcuni dati in ogni tabella */
+INSERT INTO Musei values('Uffizi','Firenze'), ('National gallery','Londra'), ('Louvre','Parigi'), ('Museo del prado','Madrid')
+INSERT INTO Artisti values('Tiziano','Ita'), ('Leonardo','Ita'), ('Van gogh','Ned'), ('Picasso','Spa')
+INSERT INTO Opere values('Guernica','Museo del prado','Picasso'), ('Venere di urbino','Uffizi','Tiziano'), ('Notte stellata','National gallery','Van gogh'), ('Gioconda','Louvre','Leonardo')
+INSERT INTO Personaggi values('Gesu',1), ('Dio',2), ('Madonna',3), ('Venere',4), ('San pietro',2)
