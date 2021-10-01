b238047d72bddb777c0a9f26fda055f61f69c94b
lorenzocravero <l.cravero.1295@vallauri.edu>
2021-10-01 22:45:36 +0200

    ese 4 terminato

--- SQL/Ese_04 Musei/Ese_04 Musei/Query/Query di compito.sql
+++ SQL/Ese_04 Musei/Ese_04 Musei/Query/Query di compito.sql
@@ -13,17 +13,55 @@ WHERE m.NomeM = 'Uffizi'
 AND a.NomeA = o.NomeA
 AND o.NomeM = m.NomeM
 
-/* i musei che conservano almeno 20 opere italiane */
+/* Per ciascun museo di Londra il numero di opere di artisti italiani ivi conservate*/
+SELECT m.NomeMuseo, count(*) as NumeroOpere
+FROM Musei m, Artisti a, Opere o
+WHERE a.NomeA = o.NomeA
+AND m.NomeMuseo = o.NomeMuseo 
+AND a.Nazionalita = 'ITA'
+AND m.citta = 'Londra'
+GROUP BY m.NomeMuseo
+
+/* Il nome dei musei di Londra che non hanno opere di Tiziano */
+SELECT m.NomeMuseo
+FROM Musei m
+WHERE m.Citta = 'Londra'
+AND NOT EXISTS (SELECT * FROM Opere o WHERE O.NomeA ='Tiziano' AND o.NomeMuseo = m.NomeMuseo)
+
+/* Il nome dei musei di Londra che hanno solo opere di Tiziano */
+SELECT m.NomeMuseo
+FROM Musei m
+WHERE m.Citta = 'Londra'
+AND EXISTS (SELECT * FROM Opere o WHERE O.NomeA ='Tiziano' AND o.NomeMuseo = m.NomeMuseo)
+
+/* Per ogni artista, il suo nome e il numero di opere presenti alla Galleria degli Uffizi */
+SELECT o.NomeA, count(*) as NumeroOpere
+FROM Opere o, Artisti a
+WHERE o.NomeA = a.NomeA AND o.NomeMuseo = 'Galleria degli Uffizi'
+GROUP BY a.NomeA
+
+/* I musei che conservano almeno 20 opere di artisti italiani */
+SELECT DISTINCT o.NomeMuseo
+FROM Opere o, Artisti a
+WHERE o.NomeA = a.NomeA
+AND a.Nazionalita = 'ITA' 
+HAVING COUNT(*)>19
+
+/* Titolo dell'opera e nome dell'artista per ogni opera di un artista italiano senza personaggio */
+SELECT a.NomeA, o.Titolo
+FROM Artisti a, Opere o, Personaggi p
+WHERE a.Nazionalita='IT'
+AND a.NomeA=o.NomeA
+AND NOT EXISTS(SELECT * FROM Personaggi p,Opere o WHERE p.Codice=o.Codice)
+
+/* *Il nome dei musei di londra che non hanno opere di artisti italiani, eccetto Tiziano */
 SELECT m.NomeM
 FROM Musei m
-WHERE 5 < (SELECT COUNT(*)
-FROM Artisti a, Opere m
-WHERE a.Nazionalita = 'Ita'
-AND m.NomeA = a.NomeA)
+WHERE  m.Citta='Londra'
+AND NOT EXISTS(SELECT * FROM Opere o,Artisti a WHERE a.Nazionalita='IT' OR o.NomeA<>'Tiziano' AND o.NomeM=m.NomeM)
 
-/* per tutte le opere che non hanno personaggi il titolo dell'opera ed il nome dei personaggi */
-SELECT DISTINCT o.Titolo, a.NomeA
-FROM Opere o, Artisti a, Personaggi p
-WHERE o.Codice NOT IN (3,4,5,2)
-
-/* 
+/* Per ogni museo, il numero di opere divise per nazionalità dell'artista*/
+SELECT o.NomeMuseo, count(*) as NumeroOpere
+FROM Opere o, Artisti a
+WHERE o.Nome = a.NomeA
+GROUP BY a.Nazionalita
