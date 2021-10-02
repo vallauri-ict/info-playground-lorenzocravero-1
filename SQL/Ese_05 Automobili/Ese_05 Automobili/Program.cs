1b7c4cb908ad844a9f5643bb6ac1446ccf075034
lorenzocravero <l.cravero.1295@vallauri.edu>
2021-10-02 13:02:43 +0200

    ese 5 iniziato

---
+++ SQL/Ese_05 Automobili/Ese_05 Automobili/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ese_05_Automobili
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esercizio 5 - Automobili");
+        }
+    }
+}
